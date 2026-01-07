024c9d33ceeb8ab93ab8c968c84248ecd66ab964
DOOH.com <patrick@dooh.com>
2026-01-07 00:01:09 +0000

    Deploy auth dependencies and main config

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 
-Amplify.configure(outputs);
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "eu-west-2_3DPPZlewZ",
+      userPoolClientId: "6cd8b80gnp238ee945qvu71d5s",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
